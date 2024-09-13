56be6fe3fef999dfe72281a713d14aa5b4d9ca03
Alessandro <jsickness1996@gmail.com>
2024-09-13 15:45:30 +0200

    apache

---
+++ Apache.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.apache;
+
+/**
+ *
+ * @author jsick
+ */
+public class Apache {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
